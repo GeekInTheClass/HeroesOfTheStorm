e66a0610f855f848b65bdb1a969988cb5b6b3235
zzoroak <zzoroak@gmail.com>
2016-10-11 16:07:05 +0900

    Add Questions

--- forexample.swift
+++ forexample.swift
@@ -5,7 +5,9 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 
 print("Q1. 숫자에 2를 곱한 값 출력")
-
+for number in numbers{
+    print(2*number)
+}
 let numbers1 = numbers.map({ $0 * 2 })
 print (numbers1)
 
@@ -13,7 +15,11 @@ print (numbers1)
 
 
 print("\nQ2. 짝수만 출력")
-
+for number in numbers{
+    if(number%2 == 0){
+        print(number)
+    }
+}
 let numbers2 = numbers.filter({ $0 % 2 == 0 })
 print (numbers2)
 
@@ -22,7 +28,13 @@ print (numbers2)
 
 
 print("\nQ3. 가장 큰 수 출력")
-
+var max_num:Int = 0;
+for number in numbers{
+    if(max_num < number){
+        max_num = number
+    }
+}
+print(max_num)
 let numbers3 = numbers.sort( > )
 print (numbers3.first!)
 
@@ -31,7 +43,10 @@ print (numbers3.first!)
 
 
 print("\nQ4. 딕셔너리 출력")
-
+var nameDic:Dictionary = ["Leonard":"Sheldon", "Penny":"Raju"]
+for (key, value) in nameDic {
+    print("\(key) : \(value)")
+}
 var nameDic:Dictionary = ["Leonard":"Sheldon", "Penny":"Raju"]
 nameDic.map({print($0.0 + ":" + $0.1)})
 
@@ -39,6 +54,8 @@ nameDic.map({print($0.0 + ":" + $0.1)})
 
 
 print("\nQ5. 이름 앞에 번호 달아서 출력")
-
+for (index, value) in names.enumerated() {
+    print("\(index + 1): \(value)")
+}
 var index = 0
 nameDic.map({index += 1;print(index, ": "+$0.1)})
